bfa938edc7cfd188077ee6fa523013eded4c50d5
Michael Romer <nl@michael-romer.de>
2011-08-09 22:57:13 +0200

    added pdepend

--- bin/codemetrics.php
+++ bin/codemetrics.php
@@ -1,6 +1,6 @@
 <?php
 echo PHP_EOL . "#####################" . PHP_EOL;
-echo "#  PHP LOC Results #" . PHP_EOL;
+echo "#  PHP LOC Results  #" . PHP_EOL;
 echo "#####################" . PHP_EOL;
 echo shell_exec("phploc ../application ../library/App") . PHP_EOL;
 
@@ -9,7 +9,14 @@ echo "#  PHP MD Results  #" . PHP_EOL;
 echo "####################" . PHP_EOL;
 echo shell_exec("phpmd ../application,../library/App text design,codesize,unusedcode,naming");
 
-echo PHP_EOL . "####################" . PHP_EOL;
+echo PHP_EOL . "#####################" . PHP_EOL;
 echo "#  PHP CPD Results  #" . PHP_EOL;
-echo "####################" . PHP_EOL;
+echo "#####################" . PHP_EOL;
 echo shell_exec("phpcpd ../application ../library/App");
+
+echo PHP_EOL . "########################" . PHP_EOL;
+echo "#  PHP Depend Results  #" . PHP_EOL;
+echo "########################" . PHP_EOL;
+echo shell_exec("pdepend --summary-xml=codemetrics/summary.xml --jdepend-chart=codemetrics/jdepend.svg --overview-pyramid=codemetrics/pyramid.svg ../application,../library/App");
+
+
