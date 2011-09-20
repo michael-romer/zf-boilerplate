d4fb1e159b207038a2f80f874a17de78ddca3ef1
Michael <mail@michael-romer.de>
2011-09-20 04:08:28 +0200

    moved wsdl caching settings to application.ini

--- public/ws/soap.php
+++ public/ws/soap.php
@@ -15,10 +15,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
 
 include "Zend/Loader/Autoloader.php";
 $l = Zend_Loader_Autoloader::getInstance();
-$l->registerNamespace('Boilerplate_');
-$l->registerNamespace('Elastica_');
-$l->registerNamespace('App_');
-include "App/Webservice/Calls.php"; //?
+include "App/Webservice/Calls.php";
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
@@ -28,21 +25,19 @@ $application = new Zend_Application(
 
 $application->bootstrap();
 
-ini_set("soap.wsdl_cache_enabled", 0);//for development
-
 if (isset($_GET['WSDL'])) {
 	$autodiscover = new \Boilerplate\Webservice\Soap\AutoDiscover();
-	$autodiscover->setUri('http://localhost:8080/ws/soap.php');
+	$autodiscover->setUri('http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/ws/soap.php');
 	$autodiscover->setClass("\\App\\Webservice\\Calls");
 	$autodiscover->handle();
 } elseif (isset($_GET['INTERNALWSDL'])) {
     $autodiscover = new Zend_Soap_AutoDiscover();
     $autodiscover->setClass('\\App\\Webservice\\Calls');
-    $autodiscover->setUri('http://localhost:8080/ws/soap.php');
+    $autodiscover->setUri('http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/ws/soap.php');
     $autodiscover->handle();
 } else {
     $options = array('soap_version' => SOAP_1_2);
-    $server = new \Boilerplate\Webservice\Soap\Server('http://localhost/ws/soap.php?INTERNALWSDL=1', $options);
+    $server = new \Boilerplate\Webservice\Soap\Server('http://'.$_SERVER['SERVER_NAME'].'/ws/soap.php?INTERNALWSDL=1', $options);
     $server->setObject(new \App\Webservice\Calls());
     $server->handle();
 }
