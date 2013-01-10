9b38279991b9726e877e766169122816ea05298f
marcelaraujo <ceceldada@gmail.com>
2013-01-10 11:16:26 -0200

    Remove unecessary php settings in index.php. Move them to application.ini

--- public/index.php
+++ public/index.php
@@ -17,21 +17,10 @@ set_include_path(implode(PATH_SEPARATOR, array(
 include "Zend/Loader/Autoloader.php";
 Zend_Loader_Autoloader::getInstance();
 
-// Making XDebug more chatty in Development Environment
-if (APPLICATION_ENV == 'development')
-{
-    ini_set('xdebug.collect_vars', 'on');
-    ini_set('xdebug.collect_params', '4');
-    ini_set('xdebug.dump_globals', 'on');
-    ini_set('xdebug.dump.SERVER', 'REQUEST_URI');
-    ini_set('xdebug.show_local_vars', 'on');
-}
-
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
 
-$application->bootstrap()
-            ->run();
+$application->bootstrap()->run();
